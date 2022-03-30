dc8b0f644beb838ca55a1604943e5deeaad20b5f
Marco Boucas <marco.boucas@hotmail.fr>
2022-03-31 00:19:15 +0200

    Tests: Add tests for the base merge model

--- tests/config_test.py
+++ tests/config_test.py
@@ -7,3 +7,4 @@ from glob import glob
 TEST_FOLDER = os.path.abspath(os.path.dirname(__file__))
 
 IMAGE_PATHS = list(glob(os.path.join(TEST_FOLDER, "test_images", "*")))
+MESSAGE_IMAGE = os.path.join(TEST_FOLDER, "test_messages", "cs.png")
